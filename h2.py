1157c440fcf991c2df5eb2ac1459196b4e77949e
yuorky <34466028+yuorky@users.noreply.github.com>
2019-04-01 19:56:40 -0400

    Add files via upload

--- h2.py
+++ h2.py
@@ -11,14 +11,15 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split
 
 #Specify font sizes for plots:
-plt.rcParams['axes.labelsize']  = 10
-plt.rcParams['legend.fontsize'] = 10
-plt.rcParams['xtick.labelsize'] = 8
-plt.rcParams['ytick.labelsize'] = 8
+plt.rcParams['axes.labelsize']  = 22
+plt.rcParams['legend.fontsize'] = 22
+plt.rcParams['xtick.labelsize'] = 22
+plt.rcParams['ytick.labelsize'] = 22
 
 seed=1234
 np.random.seed(seed)
 tf.set_random_seed(seed)
+np.random.seed(1234)
 
 plt.ion() # turn on interactive mode (for plotting)
 
@@ -29,15 +30,27 @@ x_data = np.loadtxt('x_L30.txt',dtype='int8')
 y_data = np.loadtxt('y_L30.txt',dtype='int8')
 T_data = np.loadtxt('T_L30.txt',dtype='int8')
 
-X_train, X_test, y_train, y_test = train_test_split(
-        x_data,y_data,test_size=0.1)
+P = 0.7
+mm = x_data.shape[0]
+idx_m = np.random.permutation(mm)
 
-X_train, X_val, y_train, y_val = train_test_split(
-        X_train,y_train,test_size=0.2222)
+train_idx = idx_m[0:round(P*mm)]
+validation_idx = idx_m[round(P*mm):round(0.9*mm)]
+test_idx = idx_m[round(0.9*mm):mm+1]
+
+X_train = x_data[train_idx]
+y_train = y_data[train_idx]
+
+X_val = x_data[validation_idx]
+y_val = y_data[validation_idx]
+
+X_test = x_data[test_idx]
+y_test = y_data[test_idx]
+T_test = T_data[test_idx]
 
 N = 50 # number of points per branch
 K = 2  # number of branches
-
+batch_size = 100
 
 
 ############################################################################
@@ -50,12 +63,12 @@ x = tf.placeholder(tf.float32, [None, 900]) # input data
 y = tf.placeholder(tf.int32,[None])       # labels
 
 ### Layer 1: ###
-W1 = tf.Variable( tf.random_normal([900, 100], mean=0.0, stddev=0.01, dtype=tf.float32) )
-b1 = tf.Variable( tf.zeros([100]) )
+W1 = tf.Variable( tf.random_normal([900, 120], mean=0.0, stddev=0.01, dtype=tf.float32,seed=1234) )
+b1 = tf.Variable( tf.zeros([120]) )
 z1 = tf.matmul(x, W1) + b1
 a1 = tf.nn.sigmoid( z1 )
 
-W2 = W1 = tf.Variable( tf.random_normal([100, 2], mean=0.0, stddev=0.01, dtype=tf.float32) )
+W2 = W1 = tf.Variable( tf.random_normal([120, 2], mean=0.0, stddev=0.01, dtype=tf.float32,seed=1234) )
 b2 = tf.Variable( tf.zeros([2]) )
 z2 = tf.matmul(a1,W2) + b2
 a2 = tf.nn.sigmoid(z2)
@@ -67,13 +80,13 @@ aL = a2
 y_onehot = tf.one_hot(y,depth=K) # labels are converted to one-hot representation
 eps=0.0000000001 # to prevent the logs from diverging
 cross_entropy = tf.reduce_mean(-tf.reduce_sum( y_onehot * tf.log(aL+eps) +  (1.0-y_onehot )*tf.log(1.0-aL +eps) , reduction_indices=[1]))
-cost_func = cross_entropy
+cost_func = cross_entropy + tf.contrib.layers.l2_regularizer(0.1)(W1) + tf.contrib.layers.l2_regularizer(0.1)(W2)
 
 ### Use backpropagation to minimize the cost function using the gradient descent algorithm: ###
-learning_rate  = 1.0 # hyperparameter
+learning_rate  = 0.1 # hyperparameter
 train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost_func)
 
-N_epochs = 20000 # number of times to run gradient descent
+N_epochs = 50 # number of times to run gradient descent
 
 ##############################################################################
 ################################## TRAINING ##################################
@@ -84,41 +97,27 @@ sess.run(tf.global_variables_initializer())
 epoch_list    = []
 cost_training = []
 acc_training  = []
+cost_validation = []
+acc_validation = []
 
 ############ Function for plotting: ############
 def updatePlot():
 
-    ### Generate coordinates covering the whole plane: ###
-#    padding = 0.1
-#    spacing = 0.02
-#    x1_min, x1_max = x_train[:, 0].min() - padding, x_train[:, 0].max() + padding
-#    x2_min, x2_max = x_train[:, 1].min() - padding, x_train[:, 1].max() + padding
-#    x1_grid, x2_grid = np.meshgrid(np.arange(x1_min, x1_max, spacing),
-#                         np.arange(x2_min, x2_max, spacing))
-
-    NN_output       = sess.run(aL,feed_dict={x:np.c_[x1_grid.ravel(), x2_grid.ravel()]})
-    predicted_class = np.argmax(NN_output, axis=1)
-
-    ### Plot the classifier: ###
-    plt.subplot(121)
-    plt.contourf(x1_grid, x2_grid, predicted_class.reshape(x1_grid.shape), K, alpha=0.8)
-#    plt.scatter(x_train[:, 0], x_train[:, 1], c=y_train, s=40)
-    plt.xlim(x1_grid.min(), x1_grid.max())
-    plt.ylim(x2_grid.min(), x2_grid.max())
-    plt.xlabel('x1')
-    plt.ylabel('x2')
-
     ### Plot the cost function during training: ###
-    plt.subplot(222)
-    plt.plot(epoch_list,cost_training,'o-')
+    a = plt.subplot(211)
+    a.plot(epoch_list,cost_training,'bo-',label='Training cost')
+    a.plot(epoch_list,cost_validation,'g^:',label='Validation cost')
     plt.xlabel('Epoch')
-    plt.ylabel('Training cost')
+    plt.ylabel('Cost')
+    a.legend(loc='upper right')
 
     ### Plot the training accuracy: ###
-    plt.subplot(224)
-    plt.plot(epoch_list,acc_training,'o-')
+    b=plt.subplot(212)
+    b.plot(epoch_list,acc_training,'mo-',label='Training accuracy')
+    b.plot(epoch_list,acc_validation,'c^-',label='Validation accuracy')
     plt.xlabel('Epoch')
-    plt.ylabel('Training accuracy')
+    plt.ylabel('Accuracy')
+    b.legend(loc='lower right')
 ############ End of plotting function ############
 
 ### Train for several epochs: ###
@@ -126,17 +125,26 @@ for epoch in range(N_epochs):
     sess.run(train_step, feed_dict={x: X_train,y:y_train}) #run gradient descent
     
     ### Update the plot and print results every 500 epochs: ###
-    if epoch % 500 == 0:
-        cost = sess.run(cost_func,feed_dict={x:X_train, y:y_train})
-        NN_output = sess.run(aL,feed_dict={x:X_train, y:y_train})
-        predicted_class = np.argmax(NN_output, axis=1)
-        accuracy = np.mean(predicted_class == y_train)
-    
-        print( "Iteration %d:\n  Training cost %f\n  Training accuracy %f\n" % (epoch, cost, accuracy) )
-    
+    if epoch % 1 == 0:
+        cost1 = sess.run(cost_func,feed_dict={x:X_train, y:y_train})
+        cost2 = sess.run(cost_func,feed_dict={x:X_val, y:y_val})
+        
+        NN_output_train = sess.run(aL,feed_dict={x:X_train, y:y_train})
+        predicted_class1 = np.argmax(NN_output_train, axis=1)
+        accuracy1 = np.mean(predicted_class1 == y_train)
+        
+        NN_output_val = sess.run(aL,feed_dict={x:X_val, y:y_val})
+        predicted_class2 = np.argmax(NN_output_val,axis=1)
+        accuracy2 = np.mean(predicted_class2 == y_val)
+        
+        print( "Iteration %d:\n  Training cost %f\n  Training accuracy %f\n" % (epoch, cost1, accuracy1) )
+        print( "Iteration %d:\n  Validation cost %f\n  Validation accuracy %f\n" % (epoch, cost2, accuracy2) )  
+        
         epoch_list.append(epoch)
-        cost_training.append(cost)
-        acc_training.append(accuracy)
+        cost_training.append(cost1)
+        cost_validation.append(cost2)
+        acc_training.append(accuracy1)
+        acc_validation.append(accuracy2)
         
         ### Update the plot of the resulting classifier: ###
         fig = plt.figure(2,figsize=(10,5))
@@ -145,6 +153,10 @@ for epoch in range(N_epochs):
         updatePlot()
         plt.pause(0.1)
 
-plt.savefig('spiral_results.pdf') # Save the figure showing the results in the current directory
+plt.savefig('spiral_results1.jpg') # Save the figure showing the results in the current directory
 
 plt.show()
+
+a = sess.run(aL, feed_dict={x:X_test,y:y_test})
+predicted_class3 = np.argmax(a, axis=1)
+accuracy3 = np.mean(predicted_class3 == y_test)
